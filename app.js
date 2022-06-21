85b5017e0edcdae1cf771dfb0bc4e9df9fdba6e7
rendisagala <rendisagala19@gmail.com>
2022-06-21 21:48:33 +0700

    change app.js

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running!!`);
 });
